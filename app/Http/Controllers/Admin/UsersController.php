e4146e5840d9df356847582cd7921f0a6cf6cc8e
Artemis Deepin 8GB <perudragon@hotmail.com>
2018-10-12 07:29:43 -0400

    04.1 set Middleware for a Controller
    en el middleware habia una restriccion de edad. ahora para que esto funcione debe ingresar esta url: http://127.0.0.1:8000/admin/users?age=25. si es menor de edad te devuelve a la pag principal caso contrario puedes ver el contenido de usercontroller.

--- app/Http/Controllers/Admin/UsersController.php
+++ app/Http/Controllers/Admin/UsersController.php
@@ -6,8 +6,12 @@ use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 class UsersController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('checkAge');
+    }
     public function listUsers()
     {
-        return 123;
+        return "List: users - Admin/Users controller";
     }
 }
