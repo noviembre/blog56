32c6314787bf4bffc1f2c4b6961da48d0cd5ccbe
Artemis Deepin 8GB <perudragon@hotmail.com>
2018-10-13 06:05:46 -0400

    05.2 Migration - Change a Particular Property
    changing thee default value of title.

---
+++ database/migrations/2018_10_13_100119_change_default_value_of_title.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class ChangeDefaultValueOfTitle extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('posts', function (Blueprint $table) {
+            //
+            $table->string('title')->default('Nice Title')->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('posts', function (Blueprint $table) {
+            //
+        });
+    }
+}
